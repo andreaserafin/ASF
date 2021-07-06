06d3e66d450db89e8c929db93afbdb27a19e1db7
andreaserafin <andrea.serafin@stud.tecnicosuperiorekennedy.it>
2021-07-06 09:10:24 +0200

    Edited framework
    Model viewer

--- index.js
+++
@@ -1,9 +0,0 @@
-var m = document.getElementById("eggs");
-
-m.addEventListener("markerFound", (e)=>{
-   console.log("found")
-})
-
-m.addEventListener("markerLost", (e)=>{
-   console.log("lost")
-})
